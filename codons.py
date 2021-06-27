32ec68ce02caad0851b731ba7b5d2e1fe82fd855
aamberh <86326452+aamberh@users.noreply.github.com>
2021-06-27 14:28:34 -0700

    Update codons.py

--- codons.py
+++ codons.py
@@ -5,7 +5,16 @@
 
 dna = 'ATAGCGAATATCTCTCATGAGAGGGAA'
 
-# your code goes here
+for i in range(len(dna)):
+  print(dna[:3])
+  print(dna[3:6])
+  print(dna[6:9])
+  print(dna[9:12])
+  print(dna[12:15])
+  print(dna[15:18]) 
+  print(dna[18:21])
+  print(dna[21:24])
+  print(dna[24:27])
 
 """
 python3 codons.py
